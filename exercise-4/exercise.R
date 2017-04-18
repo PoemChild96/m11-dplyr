c4ba312cf2b3d122e8b673048cbf7b8528026c91
Utako Kase <utakok@uw.edu>
2017-04-18 14:14:41 -0700

    ex4 half way thru

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,21 +2,23 @@
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
 # install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
 
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights <- mutate(flights, gained = (arr_delay - dep_delay))
+View(flights)
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, desc(gained))
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- mutate(flights, gained = (arr_delay - dep_delay)) %>% arrange(flights, desc(gained))
 
 # Make a histogram of the amount of gain using the `hist` command
 
